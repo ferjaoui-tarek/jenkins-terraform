5586dd18caa15966fb5e3721c72caf3ed7885d6c
Tarek Ferjaoui <ferjaouitarek@gmail.com>
2020-03-05 13:58:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "terraform-bucket-tarek"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
