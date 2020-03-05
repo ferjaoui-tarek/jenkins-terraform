2df99902acfa6fcbdb9106646bf56f1b0c39d8b1
Tarek Ferjaoui <t.ferjaoui@melijoe.com>
2020-03-05 09:56:17 +0100

    jenkins and terraform

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "instance_count" {
+  default = 1
+}
+
+variable "key_name" {
+  description = "Private key name to use with instance"
+  default     = "terraform"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t3.small"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+
+  # Bitnami NGINX AMI
+  default = "ami-021acbdb89706aa89"
+}
